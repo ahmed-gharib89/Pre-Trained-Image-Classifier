05bdecadea1c6411e1d462a8024c9555d9e5ea52
Ahmed Gharib <64174723+ahmed-gharib89@users.noreply.github.com>
2020-11-09 21:17:55 +0200

    Finish project requirments

--- print_results.py
+++ print_results.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
+# PROGRAMMER: Ahmed Gharib
+# DATE CREATED: 09/11/2020
 # REVISED DATE: 
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
